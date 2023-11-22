e8bdc928d29a9f901f5d15d457c0d9e7708752b2
hyun5un <144610317+hyun5un@users.noreply.github.com>
2023-11-22 23:47:02 +0900

    202111305 고현선

---
+++ hw8.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+double StandardDeviation(double number[], int size) {
+	double sum1 = 0.0;
+	double sum2 = 0.0;
+	double mo, mi;
+	for (int i = 0; i < size; i++) {
+		sum1 += number[i];
+	}
+	mo = sum1 / size;
+	for (int i = 0; i < size; i++) {
+		sum2 += pow(number[i] - mo, 2);
+	}
+	sum2 = sqrt(sum2 / size);
+	return sum2;
+}
+int main(void) {
+	double number[5];
+	int i;
+	printf("Enter 5 real numbers: ");
+	for (i = 0; i < 5; i++) {
+		scanf("%lf", &number[i]);
+	}
+	double result = StandardDeviation(number, 5);
+	printf("Standard Deviation = %.3lf", result);
+	return 0;
+}
